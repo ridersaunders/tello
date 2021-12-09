4c9655e1ccfeddafc5ddfe44d711b7095046ce13
ridersaunders <93225553+ridersaunders@users.noreply.github.com>
2021-12-09 14:49:03 -0800

    Update flightplan22.py

--- flightplan22.py
+++ flightplan22.py
@@ -7,7 +7,7 @@ tello.connect()
 print(tello.get_battery())
 
 tello.takeoff()
-tello.move_up(101)
+tello.move_up(161)
 sleep(2)
 
 tello.move_forward(152)
@@ -20,24 +20,15 @@ tello.rotate_clockwise(90)
 sleep(2)
 tello.move_down(91)
 sleep(2)
-tello.move_forward(61)
+tello.move_forward(91)
 sleep(2)
 tello.rotate_clockwise(90)
 sleep(2)
 tello.move_up(30)
 sleep(2)
+tello.move_forward(91)
+sleep(2)
 tello.rotate_counter_clockwise(90)
 sleep(2)
 tello.move_forward(182)
 sleep(2)
-
-tello.land()
-tello.end()
-
-
-
-
-
-
-
-
